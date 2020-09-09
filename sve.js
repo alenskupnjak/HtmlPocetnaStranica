e459ec76f1f17792c39f6a9e1782ad269895755b
AlenS <alenskupnjak@yahoo.com>
2020-09-09 12:28:04 +0200

    031 angular material

---
+++ sve.js
@@ -0,0 +1,7 @@
+
+    console.log(' window.location.pathname=', window.location.pathname);
+    console.log('window.location.href=', window.location.href);
+    console.log('window.location.protocol=', window.location.protocol);
+    console.log('window.location.host=', window.location.host);
+    console.log('******', window.location.href.length, window.location.pathname.length );
+    console.log(`${window.location.protocol}//${window.location.host}api/v1/users/login`);
