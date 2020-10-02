6040eb0d486dd46a7d950b56a42cbc726fd7462d
AlenS <alenskupnjak@yahoo.com>
2020-10-02 17:29:21 +0200

    v2.09 dodavani mali projekti

--- pocetnaVerzija02/js/app.js
+++ pocetnaVerzija02/js/app.js
@@ -1,4 +1,5 @@
 // promjeni boju na NAV baru
 $(window).scroll(function () {
   $('nav').toggleClass('scrolled', $(this).scrollTop() > 400);
+  $('.nav-link').toggleClass('scrolled', $(this).scrollTop() > 400);
 });
